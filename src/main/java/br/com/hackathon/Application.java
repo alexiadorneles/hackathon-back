e6857168ead559379f620a0ca08641ead7c2ce2e
eduardo.thums <eduardo.thums@cwi.com.br>
2019-10-14 18:03:37 -0300

    Commit test to verify repository validation

--- src/main/java/br/com/hackathon/Application.java
+++ src/main/java/br/com/hackathon/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author alexiadorneles
  * @email alexiadorneles02@gmail.com
  */
-
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
