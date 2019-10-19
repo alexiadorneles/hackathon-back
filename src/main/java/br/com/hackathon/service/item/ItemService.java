0399a905d87bac5d79bc44c1d2dd2bfe12ec12bf
João <jpedross1999@gmail.com>
2019-10-19 00:55:57 -0300

    Adicionar crud services utilizando generics.

---
+++ src/main/java/br/com/hackathon/service/item/ItemService.java
@@ -0,0 +1,7 @@
+package br.com.hackathon.service.item;
+
+import br.com.hackathon.domain.entity.ItemEntity;
+import br.com.hackathon.service.CRUDService;
+
+public interface ItemService extends CRUDService<ItemEntity, Long> {
+}
